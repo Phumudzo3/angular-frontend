92fc2c54a17acb370a90c1744206f5e9b55fd3a4
phumudzo03 <tshivhulas.p3@gmail.com>
2025-02-08 22:25:38 +0200

    init files

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,13 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { take } from 'rxjs';
 import { loadStripe } from '@stripe/stripe-js';
+import { environment } from '../../environments/environment'; // Import environment
 
 @Component({
   selector: 'app-home',
   standalone: false,
-
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'], // Fix typo,
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   constructor(private http: HttpClient) {}
@@ -38,22 +38,21 @@ export class HomeComponent {
         deliveryCharge: 20,
       },
     });
-    
+
     this.http
-      .post<any>('https://app-dev-juri.onrender.com/api/order/stripeCheckout', data)
+      .post<any>(`${environment.apiUrl}/api/order/stripeCheckout`, data) // Use apiUrl from environment
       .pipe(take(1))
       .subscribe({
-       next: async (session) => {
-    try {
-        console.log('Payment session created successfully:', session.id); // Log session ID for reference
-        const stripe = await loadStripe('pk_test_51O0gq3F71lqZQv1BeofSsRihCVfY63EdQT29fNod5ssZ2A3i3WKqmDjFSgCwJgfFNUuamw9nwez2TD1LAtK8mwcq00kR8VfBBc'); 
-        await stripe?.redirectToCheckout({ sessionId: session.id });
-        console.log('Redirecting to Stripe checkout...');
-    } catch (error) {
-        console.error('Error during Stripe redirect:', error);
-    }
-},
-
+        next: async (session) => {
+          try {
+            console.log('Payment session created successfully:', session.id); // Log session ID for reference
+            const stripe = await loadStripe('pk_test_51O0gq3F71lqZQv1BeofSsRihCVfY63EdQT29fNod5ssZ2A3i3WKqmDjFSgCwJgfFNUuamw9nwez2TD1LAtK8mwcq00kR8VfBBc');
+            await stripe?.redirectToCheckout({ sessionId: session.id });
+            console.log('Redirecting to Stripe checkout...');
+          } catch (error) {
+            console.error('Error during Stripe redirect:', error);
+          }
+        },
       });
   }
 }
